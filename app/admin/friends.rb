0c709f3b3d57ccfae4b3bb3fcde01046cbc0f029
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-11-16 03:09:29 +0000

    Whitelist columns in Friend for ActiveAdmin

--- app/admin/friends.rb
+++ app/admin/friends.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Friend do
 
+ permit_params :user_id1, :user_id2
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
