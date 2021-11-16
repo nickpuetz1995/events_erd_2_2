abb9d1e6a5ac09cf2b473c72843360e53c0039ec
anonymous anonymous <nick.puetz@kellogg.northwestern.edu>
2021-11-16 03:09:07 +0000

    Direct association between users and comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:comments) }
+
     it { should have_many(:rsvp_statuses) }
 
     it { should have_many(:events) }
